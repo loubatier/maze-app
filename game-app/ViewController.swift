d4bb04b7d55245b22b82f988ada02a1cc849b8e8
qmichalet <33957011+qmichalet@users.noreply.github.com>
2019-05-15 17:23:47 +0200

    Feature: added walls collision logic (#4)

--- game-app/ViewController.swift
+++ game-app/ViewController.swift
@@ -16,17 +16,20 @@ class ViewController: UIViewController, UIDynamicAnimatorDelegate, UICollisionBe
     var collision: UICollisionBehavior!
 
     @IBOutlet weak var ball: UIView!
+    @IBOutlet weak var finishArea: UIView!
+    @IBOutlet weak var wall1: UIView!
+    @IBOutlet weak var wall2: UIView!
     
     var grav: UIGravityBehavior!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-
-//        ball.frame.origin.x = self.view.frame.size.width / 2
-//        ball.frame.origin.y = self.view.frame.size.height / 2
+        
 
         ball.layer.cornerRadius = 25
+        finishArea.layer.cornerRadius = 5
+        ball.frame.origin = CGPoint(x: self.view.frame.width - ball.frame.width, y: self.view.frame.height - ball.frame.height)
         
         animator = UIDynamicAnimator(referenceView: self.view)
         animator?.delegate = self
@@ -37,6 +40,8 @@ class ViewController: UIViewController, UIDynamicAnimatorDelegate, UICollisionBe
         
         let coll = UICollisionBehavior(items: [ball])
         coll.translatesReferenceBoundsIntoBoundary = true
+        coll.addBoundary(withIdentifier: "wall1" as NSCopying, for: UIBezierPath(rect: wall1.frame))
+        coll.addBoundary(withIdentifier: "wall2" as NSCopying, for: UIBezierPath(rect: wall2.frame))
         coll.collisionDelegate = self
         animator?.addBehavior(coll)
         
@@ -51,7 +56,7 @@ class ViewController: UIViewController, UIDynamicAnimatorDelegate, UICollisionBe
             self.motion.startDeviceMotionUpdates(using: .xMagneticNorthZVertical, to: OperationQueue.main) {
                 (dm, err) in
                 if let dm = dm {
-                    self.pushBall(roll: dm.attitude.roll, pitch: dm.attitude.pitch)
+                    self.pushBall(roll: 0.2 * dm.attitude.roll, pitch: 0.2 * dm.attitude.pitch)
                 }
             }
         }
